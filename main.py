cd6de486a852d79e80856ca291c10a39a977b2e2
Curtis Schrum <cschrum@galco.com>
2024-01-23 11:40:45 -0500

    Refactor: Moved Headers out of for loop

--- main.py
+++ main.py
@@ -38,22 +38,23 @@ def main():
     print(f'{start_time} Starting Now')
 
     with open(args.read_file, 'r') as url_entries:
+        headers = {
+            'User-Agent': 'curl/8.4.0',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'cookie': ''
+        }
+
         for url in url_entries:
             url = url.strip()  # Remove newline characters
-            headers = {
-                'User-Agent': 'curl/8.4.0',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'accept-language': 'en-US,en;q=0.9',
-                'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"macOS"',
-                'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'none',
-                'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
-                'cookie': ''
-            }
             session = requests.Session()
             r = session.get(url, timeout=60, headers=headers,
                             allow_redirects=True)
